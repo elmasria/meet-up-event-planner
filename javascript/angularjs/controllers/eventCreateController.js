dd72cf575d167fd83ba66b6a26915bf3c5a4d30c
Ahmad El Masri <ahmadjelmasri@gmail.com>
2015-12-28 22:35:02 +0100

    Complete the functionality of Guest List.

--- javascript/angularjs/controllers/eventCreateController.js
+++ javascript/angularjs/controllers/eventCreateController.js
@@ -1,30 +1,67 @@
-angular.module('EventPlanner').controller('EventNewController', function () {
+angular.module('EventPlanner').controller('EventNewController', function ($filter) {
 	var controller = this;
 	controller.errorMessage = '';
-	controller.Gests = [];
-	controller.gestEmail = '';
-	controller.listOfInvitedGests = [{'useremail':'ahmad@ahmad.com'},{'useremail':'ranim@ranim.com'},{'useremail':'farah@farah.com'}];
+	controller.Guests = [];
+	controller.guestEmail = '';
+	controller.listOfAvailableMembers = [
+	{'useremail':'ahmad@ahmad.com', 'isInvited': false, 'isMember': true},
+	{'useremail':'ranim@ranim.com', 'isInvited': false,'isMember': true},
+	{'useremail':'farah@farah.com', 'isInvited': false, 'isMember': true}
+	];
 	controller.preventEvent = function(event){
 		event.preventDefault();
 	};
-	controller.toogleActive = function (event, gest) {
-		var checkactive = event.currentTarget.classList.contains('active');
+	controller.toogleActive = function (guest) {
+		var checkactive = !guest.isInvited ;
 		if (checkactive) {
-			event.currentTarget.classList.remove('active');
-			controller.Gests.splice(controller.Gests.indexOf(gest),1);
+			controller.Guests.splice(controller.Guests.indexOf(guest),1);
 		}else{
-			event.currentTarget.classList.add('active');
-			controller.Gests.push(gest);			
+			controller.Guests.push(guest);			
 		}
 	};
-	controller.removeGest = function (gest) {
-		if (controller.Gests.indexOf(gest) > -1) {
-			controller.Gests.splice(controller.Gests.indexOf(gest),1);
+	controller.removeguest = function (guest) {
+		if (typeof(guest.useremail) !== 'undefined') {
+			if (controller.Guests.indexOf(guest) > -1) {
+				if (guest.isMember) {
+					controller.listOfAvailableMembers[controller.listOfAvailableMembers.indexOf(guest)].isInvited = false;
+				}
+				controller.Guests.splice(controller.Guests.indexOf(guest),1);
+			}
 		}
 	};
 
-	controller.addNewGest = function () {
-		var gestuser = {'useremail': controller.gestEmail};
-		controller.Gests.push(gestuser);
+	controller.addNewguest = function () {
+		controller.showError = false;
+		var listOfInvitedGuest = controller.Guests;
+		var listOfAvailableMembers = controller.listOfAvailableMembers;
+		var inputGuestEmail = controller.guestEmail;
+		var existingMember = $filter('filter')(listOfAvailableMembers, inputGuestEmail);
+		var invitedUsers = $filter('filter')(listOfInvitedGuest, inputGuestEmail);
+
+		//check if input is valid email
+		if (typeof(inputGuestEmail) !== 'undefined') {
+			// input email is valid 
+			// check if guest user is invited 
+			if (invitedUsers.length > 0) {
+				controller.errorMessage = inputGuestEmail + ' available in the list';
+				controller.showError = true;
+				$('html,body').animate({scrollTop:0},'slow');
+			}else{
+				// user is not invited
+				// check if user exist as member
+				if (existingMember.length > 0) {
+					// user exists, add it to the invitation list
+					listOfInvitedGuest.push(existingMember[0]);
+					// mark user as invited,
+					existingMember[0].isInvited = true;
+					
+				}else{
+					// define new user as json data.
+					var guestuser = {'useremail': inputGuestEmail, 'isInvited': true, 'isMember': false};
+					// add new user to the list of invitation
+					listOfInvitedGuest.push(guestuser);
+				}
+			}
+		}
 	};
 });
